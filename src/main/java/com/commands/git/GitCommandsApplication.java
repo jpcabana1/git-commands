25d965a945c3c12878f5a530403ad71fda668927
jpcabana1 <jpcabana1@gmail.com>
2021-11-23 22:34:28 -0300

    initial commit

---
+++ src/main/java/com/commands/git/GitCommandsApplication.java
@@ -0,0 +1,18 @@
+package com.commands.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class GitCommandsApplication {
+
+    public static void main(String[] args) {
+        //ConfigurableApplicationContext applicationContext = SpringApplication.run(GitCommandsApplication.class, args);
+
+        System.out.println("hello world!");
+
+        //applicationContext.close();
+    }
+
+}
---
+++ src/test/java/com/commands/git/GitCommandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commands.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
